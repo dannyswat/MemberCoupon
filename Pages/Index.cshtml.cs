d704b451beadc0b1a0a98992a47e15e74c6325a9
Dannys <hinleongwat@gmail.com>
2022-09-17 13:34:42 +0800

    Add something to the main page and change color

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,22 +1,29 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MemberCoupon.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MemberCoupon.Pages
 {
-    [AllowAnonymous]
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            this.context = context;
         }
 
         public void OnGet()
         {
 
+        }
+
+        public IActionResult OnGetRedemptionCount()
+        {
+            return new JsonResult(new { count = context.Redemptions.Where(r => r.Date.Date == Constants.CurrentTime().Date).Count() });
         }
     }
 }
